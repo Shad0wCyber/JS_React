2873e99af1b70a434511e5b0ea3af10a11ea5a8c
ShadowCyber <kongfoo@yandex.ru>
2025-01-20 21:20:39 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+"use strict"
+let storeName = "MagicShop";
+let storeDescription = {
+    budget: 10000,
+    employees: ["Batman", "IronMan", "Lu Kong"],
+    products: {
+        frost: 1000,
+        fire: 1500,
+    },
+    open: true,
+}
